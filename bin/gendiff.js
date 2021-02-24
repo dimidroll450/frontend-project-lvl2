104e67535f38434eb55a954e4579799050739082
dimidroll450 <dmitriykvas@outlook.com>
2021-02-24 21:30:00 +0200

    fix description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,9 +5,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
