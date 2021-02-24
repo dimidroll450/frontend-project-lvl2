c6d18d9ee42b96e2ab587c232de703d13ae848f0
dimidroll450 <dmitriykvas@outlook.com>
2021-02-24 21:28:50 +0200

    add input arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,10 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
-program.parse();
+program.parse(process.argv);
 
 export default program;
