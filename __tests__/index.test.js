4d564f8aea1e81d961f78ed70cb495982035f5ef
dimidroll450 <dmitriykvas@outlook.com>
2021-03-02 12:06:32 +0200

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,23 @@
 import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import path from 'path';
 import genDiff from '../src/index.js';
 
-const result = `
-{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('json flat files', () => {
-  const path1 = '__tests__/__fixtures__/file1.json';
-  const path2 = '__tests__/__fixtures__/file2.json';
+const result = readFile('result.txt');
+
+test('json files', () => {
+  const path1 = getFixturePath('file1.json');
+  const path2 = getFixturePath('file2.json');
 
   expect(genDiff(path1, path2)).toStrictEqual(result);
 });
 
-test('yaml flat files', () => {
-  const path1 = '__tests__/__fixtures__/file1.yml';
-  const path2 = '__tests__/__fixtures__/file2.yml';
+test('yaml files', () => {
+  const path1 = getFixturePath('file1.json');
+  const path2 = getFixturePath('file2.json');
 
   expect(genDiff(path1, path2)).toStrictEqual(result);
 });
