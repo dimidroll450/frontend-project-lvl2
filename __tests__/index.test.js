2623796b070b9ee02d4caa6b5ac0439471cc915b
dimidroll450 <dmitriykvas@outlook.com>
2021-03-08 15:45:58 +0200

    upgrade index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,26 +5,25 @@ import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
+const cases = [
+  ['json', 'json', 'stylish'],
+  ['json', 'json', 'plain'],
+  ['yml', 'yml', 'stylish'],
+  ['yml', 'yml', 'plain'],
+];
 
-const stylishResult = readFile('stylish-result.txt');
-const plainResult = readFile('plain-result.txt');
+const expectedData = {
+  stylish: readFile('stylish-result.txt'),
+  plain: readFile('plain-result.txt'),
+};
 
-describe('JSON files', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
+describe.each(cases)('genDiff between %s and %s to %s', (type1, type2, format) => {
+  test('valid data', () => {
+    const expected = expectedData[format];
+    const path1 = getFixturePath(`file1.${type1}`);
+    const path2 = getFixturePath(`file2.${type2}`);
+    const actual = genDiff(path1, path2, format);
 
-  test('formatters', () => {
-    expect(genDiff(path1, path2)).toEqual(stylishResult);
-    expect(genDiff(path1, path2, 'plain')).toEqual(plainResult);
-  });
-});
-
-describe('YAML files', () => {
-  const path1 = getFixturePath('file1.yml');
-  const path2 = getFixturePath('file2.yml');
-
-  test('formatters', () => {
-    expect(genDiff(path1, path2)).toEqual(stylishResult);
-    expect(genDiff(path1, path2, 'plain')).toEqual(plainResult);
+    expect(actual).toEqual(expected);
   });
 });
